679d814cc97f95a35fdc4aaa294fef707222c3ff
neon_10 <rusksmi@gmail.com>
2016-04-19 15:09:47 +0000

    Add 2 Project  model tests

---
+++ spec/factories/projects.rb
@@ -0,0 +1,6 @@
+# spec/factories/projects.rb
+FactoryGirl.define do
+  factory :project do |f|
+    f.title "test project"
+  end
+end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,9 +2,15 @@
 require 'rails_helper'
 
 describe Project do
-  it "has a valid factory"
-  it "is invalid without a firstname"
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:project)).to be_valid
+  end
+  
+  it "is invalid without a title" do
+    expect(FactoryGirl.build(:project, title: nil)).not_to be_valid
+  end
   it "is invalid without a lastname"
   it "returns a contact's full name as a string"
+  
 end
 
