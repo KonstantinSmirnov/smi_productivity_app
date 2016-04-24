a2b52f3d921bc107901d24af06edc0ec66ba9b66
konstantinsmirnov <rusksmi@gmail.com>
2016-04-24 16:30:41 +0300

    Add task tests

--- test/integration/tasks_test.rb
+++ test/integration/tasks_test.rb
@@ -9,39 +9,53 @@ class Tasks < Capybara::Rails::TestCase
     login_with('alex@mail.com', '123456')
     create_project('This is a project')
   end
-  
+
   scenario "create with correct title", js: true do
     fill_in "new-task-field", with: 'Eat an apple'
     click_button "Add"
-    
+
     within ".tasks-list" do
       assert find('.task').has_text?('Eat an apple')
     end
   end
-  
+
   scenario "fails to create without title", js: true do
     create_task('')
-    
+
     assert page.has_text?('Task has not been created.')
   end
-  
+
   scenario "update task", js: true do
     create_task('take a fishing rod')
-    
-    #find(".tasks-list .task .task-options").hover.find(".edit .fa-pencil").click
-    
+
     find(".tasks-list .task .task-options .edit .fa-pencil", visible: false).trigger(:click)
     fill_in "task_title", with: 'Buy a cup'
     click_button "Update"
-    
+
     assert page.has_text?('Task has been updated.')
-    
+
+  end
+
+  scenario "fails update with empty name", js: true do
+    create_task('I like apples')
+
+    find(".tasks-list .task .task-options .edit .fa-pencil", visible: false).trigger(:click)
+    fill_in "task_title", with: ''
+    click_button "Update"
+
+    assert page.has_text?('This action failed. May be you tried to use wrong data.')
+  end
+
+  scenario "remove task", js: true do
+    create_task('Welcome to Alabama')
+
+    find(".tasks-list .task .task-options .edit .fa-pencil", visible: false).trigger(:click)
+    click_link "Delete"
+
+    assert page.has_text?('You just deleted a task')
   end
 
 
-  # it "can update a task"
-  # it "fails update a task with empty name"
-  # it "can remove a task"
   # it "can check task details"
   # it "can add task description"
   # it "can 'done' task"
